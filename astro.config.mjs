6507718694db5e7e1e246226ce37cacfa96be6bb
JErickGM <jerickgm89@gmail.com>
2022-11-27 07:19:25 -0500

    config astro.config base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.jerickgm.me',
-  base: '/basicReact-Astro-Tailwing',
+  base: '/',
   integrations: [react(), tailwind()]
 });
